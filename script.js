f2e4a61115ea6805691a73913c49fbccf13b0604
Eduardo Kusznieryk <eduardokusznieryk@gmail.com>
2021-08-05 17:48:51 -0300

    i think that now the js file is finished. And starting with the styles

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ let div = document.createElement("div");
 
 for (let i = 0; i < 10; i++) {
   fetchAndAddToTheNode(API, "div", (data, element) => {
-    element.innerHTML = `<h3>${data.activity}</h3>`;
-    element.innerHTML += `<h4> type: ${data.type}</h4>`;
-    element.innerHTML += `<p>posible with ${data.participants} participants</p>`;
+    element.innerHTML = templateString(data);
+
     element.classList.add(data.type);
+    if (i < 2) {
+      element.classList.add("first-two");
+    }
     div.appendChild(element);
   });
 }
@@ -15,7 +17,15 @@ for (let i = 0; i < 10; i++) {
 async function fetchAndAddToTheNode(api, element, cb) {
   let data = await fetch(api).then((response) => response.json());
   let createdElement = document.createElement(element);
+
+  createdElement.classList.add("activity");
   cb(data, createdElement);
+}
+
+function templateString({ activity, type, participants }) {
+  return `<h3>${activity}</h3>
+  <h4> type: ${type}</h4>
+  <p>posible with ${participants} participants</p>`;
 }
 
 app.appendChild(div);
